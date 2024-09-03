a464ebb9b167fbd53f0eac03c78fb7d1fa978325
ozan-kaplan <ozan.kaplan@outlook.com>
2024-09-03 15:23:45 +0300

    Delete Role Command and Handler implemented.

---
+++ IoTUserService/src/IoTUserService.Application/Features/RoleCQ/Commands/DeleteRole/DeleteRoleCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoTUserService.Application.Features.RoleCQ.Commands.DeleteRole
+{
+    public class DeleteRoleCommand: IRequest<bool>
+    {
+        public DeleteRoleCommand(Guid id)
+        {
+            Id = id;
+        }
+        public Guid Id { get; set; }
+    }
+}
---
+++ IoTUserService/src/IoTUserService.Application/Features/RoleCQ/Commands/DeleteRole/DeleteRoleCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using IoTUserService.Domain.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoTUserService.Application.Features.RoleCQ.Commands.DeleteRole
+{
+    public class DeleteRoleCommandHandler : IRequestHandler<DeleteRoleCommand, bool>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public DeleteRoleCommandHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<bool> Handle(DeleteRoleCommand request, CancellationToken cancellationToken)
+        {
+
+            var entity = await _unitOfWork.Roles.GetByIdAsync(request.Id);
+
+            if (entity == null)
+                throw new Exception("Role not found!");
+
+            entity.UpdatedAt = DateTime.Now;
+            entity.IsDeleted = true;
+
+            _unitOfWork.Roles.Update(entity);
+            await _unitOfWork.SaveChangesAsync();
+
+            return true;
+
+        }
+    }
+}
